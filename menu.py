367ae1906bc5869147cf7bbc1db9b710d3f367f4
Tom Kawchak <tkawchak@gmail.com>
2015-05-07 22:43:22 -0400

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,27 @@
+import os
+
+
+def menu(options, whatToDo="what would you like to do?", clearScreenBefore = True, clearScreenAfter = True):
+    """basic menu for cmd line menu text.
+        @param options (string[]) - array of all options for the menu
+        @param whatToDo (string) - prompt for the menu
+        @param clearScreenBefore (bool) - if True, screen is cleared before menu is displayed
+        @param clearScreenAfter (bool) - if True, screen is cleared after user input is read from menu choice
+        @return (string) - user choice for menu
+    """
+
+    if clearScreenBefore:
+        os.system("cls")
+
+    # DISPLAYS THE MENU OPTIONS
+    print(whatToDo)
+    for option in range(0, len(options)):
+        print("\t%d) %s" % ((option + 1), options[option]))
+
+    # GETS THE USER INPUT FROM THE MENU
+    action = raw_input("\n> ")
+
+    if clearScreenAfter:
+        os.system("cls")
+
+    return action
